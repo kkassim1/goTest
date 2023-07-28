75b29f874f0b91058e42f141c49af91cd7b490a1
kkassim1 <97631431+kkassim1@users.noreply.github.com>
2023-07-28 15:55:45 -0700

    Integerbranch (#3)
    * starting integer section
    
    * testing rep char
    
    * testing benchmark
    
    * updted readme
    
    * sum good needs range refactor
    
    * code cleanup
    
    * e
    
    ---------
    
    Co-authored-by: kwam kassim <kkassim@asu.edu>
    Co-authored-by: Kwam Kassim <kkassim@starbucks.com>

---
+++ integers/adder.go
@@ -0,0 +1,19 @@
+package integers
+
+func repateChar(letter string, count int) string {
+	tab := ""
+
+	for i := 0; i < count; i++ {
+
+		tab += letter
+
+	}
+
+	return tab
+}
+
+func Add(a, b int) int {
+
+	return a + b
+
+}
---
+++ sumTest/sum.go
@@ -0,0 +1,16 @@
+package main
+
+func sumAll(s1 []int, s2 []int) []int {
+
+	return nil
+
+}
+
+func sum(numArr []int) int {
+	sum := 0
+
+	for _, number := range numArr {
+		sum += number
+	}
+	return sum
+}
