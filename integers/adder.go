0b2193a05de839ab46f623b3652c1522bcfd33a4
Kwam Kassim <kkassim@starbucks.com>
2023-07-28 14:10:12 -0700

    testing rep char

--- integers/adder.go
+++ integers/adder.go
@@ -1,5 +1,17 @@
 package integers
 
+func repateChar(letter string, count int) string {
+	tab := ""
+
+	for i := 0; i < count; i++ {
+
+		tab += letter
+
+	}
+
+	return tab
+}
+
 func Add(a, b int) int {
 
 	return a + b
--- integers/adder_test.go
+++ integers/adder_test.go
@@ -2,6 +2,22 @@ package integers
 
 import "testing"
 
+func TestRepeate(t *testing.T) {
+
+	t.Run("test repeate characther 5 times", func(t *testing.T) {
+
+		got := repateChar("p", 6)
+		want := "pppppp"
+
+		if got != want {
+
+			t.Errorf("got %q but want %q", got, want)
+		}
+
+	})
+
+}
+
 func TestAdder(t *testing.T) {
 
 	t.Run("test adder", func(t *testing.T) {
