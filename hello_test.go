c6dfeb1e84d29e7570b8ca758041aadba24115f6
kwam kassim <kkassim@asu.edu>
2023-07-27 18:59:48 -0700

    first test ingo tdd

---
+++ hello_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	got := Hello("chris")
+	want := "hello,chris"
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+
+}
