e211a5a13918ea51eaf48ba59f560612ebbe35a3
kwam kassim <kkassim@asu.edu>
2023-07-30 19:48:23 -0700

    summall

--- sumTest/sum_test.go
+++ sumTest/sum_test.go
@@ -7,8 +7,8 @@ import (
 
 func TestSumAll(t *testing.T) {
 
-	got := SumAll([]int{1, 2}, []int{0, 9})
-	want := []int{3, 9}
+	got := SumAll([]int{2, 2}, []int{0, 9})
+	want := []int{4, 9}
 
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v want %v", got, want)
