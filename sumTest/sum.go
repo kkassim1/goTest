48fa21c18c55a6e6e90a73e111f380c9cb660300
kkassim1 <barry_kwam20@yahoo.com>
2023-08-03 12:32:49 -0700

    .

--- sumTest/sum.go
+++ sumTest/sum.go
@@ -5,12 +5,15 @@ import (
 	"math"
 )
 
-type Rectangle struct {
-	Width  float64
-	Height float64
+type Shape interface {
+	Area() float64
 }
 type Circle struct {
 	Radius float64
+}
+type Rectangle struct {
+	Width  float64
+	Height float64
 }
 
 func (c Circle) Area() float64 {
