8a94938a94c343e0fc7c770e2e8872ee7c5d660d
kkassim1 <barry_kwam20@yahoo.com>
2023-08-21 16:46:22 -0700

    adding withdraw

--- pointersNErrors/pointErr.go
+++ pointersNErrors/pointErr.go
@@ -1,8 +1,22 @@
 package pointersnerrors
 
+import "fmt"
+
 type Bitcoin int
 type Wallet struct {
 	balance Bitcoin
+}
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+
+	w.balance = w.balance - amount
 }
 
 func (w *Wallet) Deposit(amount Bitcoin) {
