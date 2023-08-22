313d2be1cc685bdec360ef842d12d239088becc3
kkassim1 <barry_kwam20@yahoo.com>
2023-08-21 20:03:35 -0700

    first map test

--- maps/maps.go
+++ maps/maps.go
@@ -1,5 +1,7 @@
 package main
 
-func Search(k map[string]string, g string) string {
-	return "this is just a test"
+type Dictionary map[string]string
+
+func (d Dictionary) Search(word string) string {
+	return d[word]
 }
