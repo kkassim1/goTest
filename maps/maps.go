efdd0cda232ee409608adbecae1d5eee4e49d21b
Kwam Kassim <kkassim@starbucks.com>
2023-08-23 15:31:56 -0700

    maos

--- maps/maps.go
+++ maps/maps.go
@@ -4,19 +4,26 @@ import "errors"
 
 type Dictionary map[string]string
 
-var ErrCantFinf = "could not find the word you were looking for"
+var (
+	ErrCantFinf   = errors.New("could not find the word you were looking for")
+	ErrWordExists = errors.New("cannot add word because it already exists")
+)
 
-func (d Dictionary) Add(word, definition string) {
+func (d Dictionary) Add(word, definition string) error {
 
+	if _, err := d.Search(d[word]); err == nil {
+
+		return ErrWordExists
+	}
 	d[word] = definition
-
+	return nil
 }
 func (d Dictionary) Search(word string) (string, error) {
 
 	definition, ok := d[word]
 
 	if !ok {
-		return "", errors.New(ErrCantFinf)
+		return "", ErrCantFinf
 	}
 
 	return definition, nil
--- maps/maps_test.go
+++ maps/maps_test.go
@@ -3,23 +3,33 @@ package main
 import "testing"
 
 func TestAdd(t *testing.T) {
-
-	t.Run("add word", func(t *testing.T) {
-
+	t.Run("new word", func(t *testing.T) {
 		dictionary := Dictionary{}
 		word := "test"
 		definition := "this is just a test"
 
-		dictionary.Add(word, definition)
+		err := dictionary.Add(word, definition)
 
+		assertError(t, err, nil)
 		assertDefinition(t, dictionary, word, definition)
-
 	})
 
-	t.Run("dont add if word already exist", func(t *testing.T) {
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
 
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
 	})
+}
 
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
 
 func TestSearch(t *testing.T) {
@@ -34,7 +44,7 @@ func TestSearch(t *testing.T) {
 
 	t.Run("unknown word", func(t *testing.T) {
 		_, err := dictionary.Search("unknown")
-		want := ErrCantFinf
+		want := "could not find the word you were looking for"
 
 		if err == nil {
 			t.Fatal("expected to get an error.")
@@ -49,13 +59,6 @@ func assertStrings(t testing.TB, got, want string) {
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
-	}
-}
-func assertError(t testing.TB, got, want error) {
-	t.Helper()
-
-	if got != want {
-		t.Errorf("got error %q want %q", got, want)
 	}
 }
 
