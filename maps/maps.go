2444e0a987c6ff5ad2a00c0ca256f1d7481214a4
kkassim1 <barry_kwam20@yahoo.com>
2023-08-22 17:30:39 -0700

    unknown word

--- maps/maps.go
+++ maps/maps.go
@@ -4,12 +4,19 @@ import "errors"
 
 type Dictionary map[string]string
 
+var ErrCantFinf = "could not find the word you were looking for"
+
+func (d Dictionary) Add(word, definition string) {
+
+	d[word] = definition
+
+}
 func (d Dictionary) Search(word string) (string, error) {
 
 	definition, ok := d[word]
 
 	if !ok {
-		return "", errors.New("could not find the word you were looking for")
+		return "", errors.New(ErrCantFinf)
 	}
 
 	return definition, nil
--- maps/maps_test.go
+++ maps/maps_test.go
@@ -2,8 +2,21 @@ package main
 
 import "testing"
 
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	dictionary.Add("test", "this is just a test")
+
+	want := "this is just a test"
+	got, err := dictionary.Search("test")
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	assertStrings(t, got, want)
+}
+
 func TestSearch(t *testing.T) {
-	dictionary := Dictionary{"test": "this is just a test"}
+	dictionary := Dictionary{"test": "this is just a test", "t": "k"}
 
 	t.Run("known word", func(t *testing.T) {
 		got, _ := dictionary.Search("test")
@@ -14,7 +27,7 @@ func TestSearch(t *testing.T) {
 
 	t.Run("unknown word", func(t *testing.T) {
 		_, err := dictionary.Search("unknown")
-		want := "could not find the word you were looking for"
+		want := ErrCantFinf
 
 		if err == nil {
 			t.Fatal("expected to get an error.")
@@ -29,5 +42,12 @@ func assertStrings(t testing.TB, got, want string) {
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
+}
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
 	}
 }
